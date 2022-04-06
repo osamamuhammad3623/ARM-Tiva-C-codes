e0a2cb961c18072ca977cc6a24c3c734136a4da6
osamamuhammad3623 <osamamohammed3623@gmail.com>
2022-04-06 05:18:28 +0200

    finish basic gpio

---
+++ basic gpio/basic_gpio.c
@@ -0,0 +1,331 @@
+#include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\tm4c123gh6pm.h"
+#include <stdint.h>
+#include "basic_gpio.h"
+#include "common_macros.h"
+
+void GPIO_init(const GPIO_Configuration * config){
+    SET_BIT(SYSCTL_RCGCGPIO_R, config->port); /* enable clock for the port */
+    if (config->port == PORTA){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTA_DIR_R, config->pin);
+
+            if (config->data){ SET_BIT(GPIO_PORTA_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTA_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTA_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTA_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTA_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTA_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTA_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTA_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTA_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTA_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTA_DEN_R, config->pin);
+        }
+    }
+
+    else if (config->port == PORTB){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTB_DIR_R, config->pin);
+
+            if (config->data){ SET_BIT(GPIO_PORTB_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTB_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTB_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTB_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTB_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTB_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTB_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+            
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTB_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTB_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTB_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTB_DEN_R, config->pin);
+        }
+    }
+
+    else if (config->port == PORTC){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTC_DIR_R, config->pin);
+
+            if (config->data){ SET_BIT(GPIO_PORTC_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTC_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTC_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTC_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTC_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTC_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTC_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+            
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTC_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTC_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTC_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTC_DEN_R, config->pin);
+        }
+    }
+
+    else if (config->port == PORTD){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTD_DIR_R, config->pin);
+
+            if (config->data){ SET_BIT(GPIO_PORTD_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTD_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTD_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTD_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTD_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTD_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTD_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+            
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTD_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTD_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTD_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTD_DEN_R, config->pin);
+        }
+    }
+
+    else if (config->port == PORTE){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTE_DIR_R, config->pin);
+
+            if (config->data){ SET_BIT(GPIO_PORTE_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTE_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTE_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTE_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTE_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTE_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTE_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+            
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTE_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTE_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTE_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTE_DEN_R, config->pin);
+        }
+    }
+
+    else if (config->port == PORTF){
+        /* Configurate direction & data */
+        if (config->dir == Output){
+            SET_BIT(GPIO_PORTF_DIR_R, config->pin);
+
+            if (config->data == LOGIC_HIGH){ SET_BIT(GPIO_PORTF_DATA_R, config->pin);}
+            else{ CLEAR_BIT(GPIO_PORTF_DATA_R, config->pin);}
+
+        }else{
+            CLEAR_BIT(GPIO_PORTF_DIR_R, config->pin);
+        }
+
+        /* Configurate function type */
+        if (config->type == GPIO){
+            CLEAR_BIT(GPIO_PORTF_AFSEL_R, config->pin);
+        }else {
+            SET_BIT(GPIO_PORTF_AFSEL_R, config->pin);
+        }
+
+        /* Configurate internal resistor */
+        if (config->internalR != Disable_internal_resistor){
+
+            GPIO_PORTF_LOCK_R = 0x4C4F434B; /* unlock commit reg. */
+            SET_BIT(GPIO_PORTF_CR_R,config->pin) ; /* enable commit reg. to control PUR/PDR reg. */
+            
+            if (config->internalR == Pull_up_resistor){ 
+                SET_BIT(GPIO_PORTF_PUR_R, config->pin); /* enable PUR for the pin */
+            }else{
+                SET_BIT(GPIO_PORTF_PDR_R, config->pin); /* enable PDR for the pin */
+            }
+        }
+
+        /* Configurate Digital IO */
+        if (config->isDigital == Digital){
+            SET_BIT(GPIO_PORTF_DEN_R, config->pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTF_DEN_R, config->pin);
+        }
+    }
+}
+
+void GPIO_writeData(GPIO_Port port, GPIO_Pin pin, uint8_t data){
+    switch (port)
+    {
+    case PORTA:
+        if (data){
+            SET_BIT(GPIO_PORTA_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTA_DATA_R, pin);
+        }
+        break;
+    
+    case PORTB:
+        if (data){
+            SET_BIT(GPIO_PORTB_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTB_DATA_R, pin);
+        }
+        break;
+    
+    case PORTC:
+        if (data){
+            SET_BIT(GPIO_PORTC_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTC_DATA_R, pin);
+        }
+        break;
+
+    case PORTD:
+        if (data){
+            SET_BIT(GPIO_PORTD_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTD_DATA_R, pin);
+        }
+        break;
+    
+    case PORTE:
+        if (data){
+            SET_BIT(GPIO_PORTE_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTE_DATA_R, pin);
+        }
+        break;
+    
+    case PORTF:
+        if (data){
+            SET_BIT(GPIO_PORTF_DATA_R, pin);
+        }else{
+            CLEAR_BIT(GPIO_PORTF_DATA_R, pin);
+        }
+        break;
+    
+    default:
+        break;
+    }
+}
+
+uint8_t GPIO_readData(GPIO_Port port, GPIO_Pin pin){
+
+    switch (port)
+    {
+    case PORTA:
+        return (GPIO_PORTA_DATA_R & (1<<pin));
+    
+    case PORTB:
+        return (GPIO_PORTB_DATA_R & (1<<pin));
+    
+    case PORTC:
+        return (GPIO_PORTC_DATA_R & (1<<pin));
+    
+    case PORTD:
+        return (GPIO_PORTD_DATA_R & (1<<pin));
+    
+    case PORTE:
+        return (GPIO_PORTE_DATA_R & (1<<pin));
+
+    
+    case PORTF:
+        return (GPIO_PORTF_DATA_R & (1<<pin));
+    
+    default:
+        break;
+    }
+}
