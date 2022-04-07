2e22ebe8a17a2fdb36f871577064bc081185d1da
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2022-04-07 21:29:57 +0200

    Update basic_timer_toggle_led.c

--- basic_timer_toggle_led.c
+++ basic_timer_toggle_led.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
 #include "C:\ti\TivaWare_C_Series-2.1.4.178\inc\tm4c123gh6pm.h"
-#include <stdint.h>
-#include "builtin_led.h"
 #include "basic_timer.h"
+#include "builtin_led.h"
+
+
+void turnOnLED(void){
+	LED_Toggle(RED);
+}
 
 int main(void){
 		
-		LED_Configure(RED);
-		LED_Disable();
-		
-		Timer_configuration timer_config = {Timer0, one_shot, Count_down, 0x10};
-		Timer_init(&timer_config);	
+	LED_Configure(RED);
 	
-		while(1){
-			if (Timer_is_time_out(Timer0)){
-				LED_Toggle(RED);
-			}
+	Timer_configuration timer_config = {Timer2, periodic, Count_down, 0x1F, turnOnLED};
+	Timer_init(&timer_config);	
 
-		}
+	while(1);
+	
+	return 0;
 }
